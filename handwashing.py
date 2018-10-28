70316f48a21467d9272307a31e8a6a3564444267
Javid Mahmudov <Jav1d@users.noreply.github.com>
2018-10-28 16:56:58 +0400

    Delete handwashing.py

--- handwashing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-yearly = pd.read_csv("datasets/yearly_deaths_by_clinic.csv")
-
-yearly["proportion_deaths"] = yearly["deaths"] / yearly["births"]
-yearly1 = yearly[yearly["clinic"] == 'clinic 1']
-yearly2 = yearly[yearly["clinic"] == 'clinic 2']

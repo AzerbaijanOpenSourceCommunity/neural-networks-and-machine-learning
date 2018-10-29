a9e3875c03de7124c8a07c080a72242ac0883bb1
Javid Mahmudov <Jav1d@users.noreply.github.com>
2018-10-29 09:50:14 +0400

    Update handwashing.py

--- handwashing problem/handwashing.py
+++ handwashing problem/handwashing.py
@@ -5,3 +5,6 @@ yearly = pd.read_csv("datasets/yearly_deaths_by_clinic.csv")
 yearly["proportion_deaths"] = yearly["deaths"] / yearly["births"]
 yearly1 = yearly[yearly["clinic"] == 'clinic 1']
 yearly2 = yearly[yearly["clinic"] == 'clinic 2']
+
+
+#IT HAS NOT COMPLETED YET

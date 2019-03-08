37c5e5e1352df2e449622f2758f49f0cc6ab6c45
Javid Mahmudov <Jav1d@users.noreply.github.com>
2019-03-08 23:16:55 +0400

    First Commit for final version of Linear Regressor

--- Linear-Regression/linear_regression_keras.py
+++ Linear-Regression/linear_regression_keras.py
@@ -3,17 +3,23 @@ import pandas as pd
 from keras.layers import Dense
 from keras.optimizers import Adam, SGD
 from sklearn.datasets import load_boston
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('...Linear-Regression/test.csv')
+df = pd.read_csv("datasets/weight-height.csv")
+X = df[['Height']].values
+y = df[['Weight']].values
 
 model = Sequential(0)
-model.add(Dense(1, input_shape=(len(X.columns),)))
+model.add(Dense(1, input_shape=(1,)))
 print(model.summary())
 model.compile(Adam(lr=0.8), 'mean_squared_error')
-model.fit(X, y, epochs=50)
+model.fit(X, y, epochs=40)
 
 y_pred = model.predict(X)
 
 df.plot(kind='scatter',
-             x='Features',
-        y='Price')
+
+        x='Height',
+        y='Weight')
+
+plt.plot(X, y_pred, color='red')
